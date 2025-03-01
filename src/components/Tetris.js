b6716b70a962091dd16444f6ace42d54c7e8429e
rachelofyork <rachelofyork@gmail.com>
2025-03-01 19:22:32 +0000

    styled components

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -3,20 +3,24 @@ import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
 import { createStage } from '../gameHelpers';
+import { StyledTetrisWrapper} from './styles/StyledTetris';
+import { StyledTetris } from './styles/StyledTetris';
 
 const Tetris = () => {
   return (
-    <div>
-      <Stage stage={createStage()} />
-      <aside>
+    <StyledTetrisWrapper>
+      <StyledTetris>
+       <Stage stage={createStage()} />
+        <aside>
         <div>
           <Display text="Score" />
           <Display text="Rows" />
           <Display text="Level" />
         </div>
         <StartButton />
-      </aside>
-    </div>
+        </aside>
+       </StyledTetris>
+    </StyledTetrisWrapper>
   );
 };
 
